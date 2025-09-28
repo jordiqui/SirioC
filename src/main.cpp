d375c5975b69dc91e5cfebd9f8dac73359e7efbd
Jorge Ruiz Centelles <51847413+jordiqui@users.noreply.github.com>
2025-09-28 19:26:08 +0200

    Refine syzygy support and clean UCI interface

--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,10 @@
 #include "engine/uci/uci.hpp"
 #include "engine/config.hpp"
 
-namespace engine {
-class Engine {
-public:
-    Engine() = default;
-    void run() {
-        Uci uci(*this);
-        uci.loop();
-    }
-    // UCI will call these:
-    void on_newgame() {}
-};
-} // namespace engine
-
 int main() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
-    engine::Engine e;
-    e.run();
+    engine::Uci uci;
+    uci.loop();
     return 0;
 }
