e33af3df55778a613b3e6f975f57945d7997a78d
Jorge Ruiz Centelles <51847413+jordiqui@users.noreply.github.com>
2025-10-02 21:20:57 +0200

    Delete device directory

--- device/src/timeman.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "search.h"
-
-namespace TimeMan {
-
-  void calcOptimumTime(const Search::Settings& settings, Color us,
-                       int64_t* optimumTime, int64_t* maximumTime);
-}
